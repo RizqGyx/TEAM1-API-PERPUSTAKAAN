f36dcab6c591daa4a394968c2f586de0987d6d25
RizqGyx <mhdrizky32@gmail.com>
2024-04-13 17:19:50 +0700

    Adding: Innitial Project

---
+++ bin/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+const router = require("../routes/index");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.use(flash());
+app.use(morgan("dev"));
+app.use(session({ secret: "team1", saveUninitialized: true, resave: true }));
+app.use(express.static(`${__dirname}/public`));
+
+app.use(router);
+
+module.exports = app;
