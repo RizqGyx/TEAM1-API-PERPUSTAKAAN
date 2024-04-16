61972c0d225f14a39b24a6f5f0b44ec89f2e3480
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-04-16 22:00:34 +0700

    Update : routes/index

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+// const Admin = require("./adminRouter");
+const Library = require("./libraryRouter");
+
+// router.use("/", Admin);
+router.use("/api/v1/librarys", Library);
+
+module.exports = router;
