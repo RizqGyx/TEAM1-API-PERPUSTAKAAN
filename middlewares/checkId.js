5be5198c48446b31bac0dfc8bced12af67c7c9cf
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-04-16 22:01:34 +0700

    create middlewares

---
+++ middlewares/checkId.js
@@ -0,0 +1,18 @@
+const ApiError = require("../utils/apiError");
+const { Library } = require("../models");
+
+const checkId = async (req, res, next) => {
+    try {
+        const library = await Library.findByPk(req.params.id);
+
+        if (!library) {
+            return next(new ApiError(`library gak ada`, 404));
+        }
+
+        next();
+    } catch (err) {
+        next(new ApiError(err.message, 500));
+    }
+};
+
+module.exports = checkId;
