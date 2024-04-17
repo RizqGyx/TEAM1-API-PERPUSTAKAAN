ba8388f62bc157b39dd7ff4dd631696b09b10975
MAULANA TEGAR IBRAHIM <tegarmaulana973@gmail.com>
2024-04-18 00:09:40 +0700

    add middlewares uploader

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
+const upload = require("../middlewares/uploader");
 const Authenticate = require("../middlewares/authenticate");
 const Auth = require("../controller/authController");
 const checkRole = require("../middlewares/checkRole");
 
 router.post("/login", Auth.login);
 router.post("/staff/register", Authenticate, Auth.registerStaff);
-router.post("/admin/register", Authenticate, checkRole("Admin"), Auth.createAdmin);
+router.post("/admin/register", Authenticate, checkRole("Admin"), upload.array("imgUrl"),Auth.createAdmin);
 // router.post("/admin/register", Auth.createAdmin);
 module.exports = router;
