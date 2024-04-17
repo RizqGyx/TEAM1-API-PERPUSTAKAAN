32d2fedccb82b96913c11277302fea5b4efb7951
MAULANA TEGAR IBRAHIM <tegarmaulana973@gmail.com>
2024-04-17 13:05:17 +0700

    Modified chechRole Admin/user

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,11 +1,14 @@
 const ApiError = require("../utils/apiError");
 
-const checkRole = (role) => {
+const checkRole = (...roles) => {
   return async (req, res, next) => {
     try {
-      if (req.user.role !== role) {
-        next(new ApiError(`kamu bukan ${role} jadi tidak bisa akses`, 401));
+      // Periksa apakah peran pengguna ada dan ada di antara daftar peran yang diizinkan
+      if (!req.user || !roles.includes(req.user.role)) {
+        next(new ApiError(`Kamu tidak memiliki akses untuk ini`, 401));
+        return;
       }
+      // Lanjutkan jika peran pengguna sesuai dengan salah satu dari peran yang diizinkan
       next();
     } catch (err) {
       next(new ApiError(err.message, 500));
