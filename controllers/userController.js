054b9994f89f587240adb3737f647913c85fd694
MAULANA TEGAR IBRAHIM <tegarmaulana973@gmail.com>
2024-04-18 18:57:09 +0700

    Adding: finduserfilter,search,pagination

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,31 +4,25 @@ const ApiError = require("../utils/apiError");
 
 const findUsers = async (req, res, next) => {
   try {
-    const users = await User.findAll();
+    // const users = await User.findAll();
+    let filters = {};
 
-    res.status(200).json({
-      status: "Success",
-      data: {
-        users,
-      },
+    const { page = 1, limit = 10 } = req.query;
+    const offset = (page - 1) * limit;
+
+    const users = await User.findAndCountAll({
+      where: filters,
+      offset,
+      limit,
     });
-  } catch (err) {
-    next(new ApiError(err.message, 400));
-  }
-};
-
-const findUserById = async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.params.id);
-
-    if (!user) {
-      next(new ApiError("User with this ID not found", 404));
-    }
 
     res.status(200).json({
       status: "Success",
       data: {
-        user,
+        users: users.rows,
+        totalItems: users.count,
+        totalPages: Math.ceil(users.count / limit),
+        currentPage: page,
       },
     });
   } catch (err) {
@@ -76,6 +70,26 @@ const findUsersByFilter = async (req, res, next) => {
     next(new ApiError(err.message, 400));
   }
 };
+
+const findUserById = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      next(new ApiError("User with this ID not found", 404));
+    }
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
 
 const updateUser = async (req, res, next) => {
   const { name, city, address, phone, role, profileImage, libraryId } =
