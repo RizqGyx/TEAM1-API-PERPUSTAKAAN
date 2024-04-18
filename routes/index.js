bd3810c24b9fd77fedc11f897c4d264bf165d423
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-04-18 23:12:28 +0700

    Fixing : router

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 
 // const Admin = require("./adminRouter");
-const Transaction = require("./transactionRouter");
+
 const Auth = require("./authRouter");
 const User = require("./userRouter");
 const Library = require("./libraryRouter");
 
 // router.use("/", Admin);
-router.use("/api/v1/transaction", Transaction);
+
 router.use("/api/v1/auth", Auth);
 router.use("/api/v1/user", User);
 router.use("/api/v1/librarys", Library);
