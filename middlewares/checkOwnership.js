ecd9398e3edf173ae9977dd58549aaba68708305
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-04-22 15:19:28 +0700

    Fixing : Role on checkOwnership

--- middlewares/checkOwnership.js
+++ middlewares/checkOwnership.js
@@ -2,10 +2,12 @@ const ApiError = require("../utils/apiError");
 const { Library, User } = require("../models");
 
 const checkOwnership = async (req, res, next) => {
+
+
   const library = await Library.findByPk(req.params.id);
 
   if (!library || (library.userId && library.userId !== req.user.id)) {
-    return next(new ApiError("You don't have access to this product", 401));
+    return next(new ApiError("You don't have access", 401));
   }
   next();
 };
