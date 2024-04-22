6351ea0b14afd8322ba174ee2c5303b9926736e3
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-04-22 15:19:44 +0700

    Fixing : Route Library

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
 const router = require("express").Router();
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("../docs/swagger.json");
+
+router.use("/api-docs", swaggerUI.serve);
+router.use("/api-docs", swaggerUI.setup(swaggerDocument));
 
 // const Admin = require("./adminRouter");
-
+const Transaction = require("./transactionRouter");
+const Rack = require("./rackRouter");
 const Auth = require("./authRouter");
 const User = require("./userRouter");
+const Book = require("./bookRouter")
 const Library = require("./libraryRouter");
 
 // router.use("/", Admin);
-
+router.use("/api/v1/transaction", Transaction);
+router.use("/api/v1/rack", Rack);
 router.use("/api/v1/auth", Auth);
 router.use("/api/v1/user", User);
-router.use("/api/v1/librarys", Library);
+router.use("/api/v1/library", Library);
+router.use("/api/v1/book", Book)
 
 module.exports = router;
--- routes/libraryRouter.js
+++ routes/libraryRouter.js
@@ -4,14 +4,38 @@ const Library = require("../controllers/libraryController");
 const autentikasi = require("../middlewares/authenticate");
 const checkId = require("../middlewares/checkId");
 const checkOwnership = require("../middlewares/checkOwnership");
-const checkRole = require("../middlewares/checkRole")
+const checkRole = require("../middlewares/checkRole");
 
 router.get("/", autentikasi, Library.findLibrarys);
-router.post("/", autentikasi, Library.createLibrary);
-router.get("/:id", checkId, autentikasi, checkOwnership, checkRole(["Admin", "Manager"]), Library.findLibraryById);
-router.patch("/:id", checkId, autentikasi, checkOwnership, checkRole(["Admin", "Manager"]), Library.updateLibrary);
-router.delete("/:id", checkId, autentikasi, checkOwnership, checkRole(["Admin", "Manager"]), Library.deleteLibrary);
-
-
+router.post(
+  "/create",
+  autentikasi,
+  checkRole(["Admin", "Manager"]),
+  Library.createLibrary
+);
+router.get(
+  "/:id",
+  autentikasi,
+  checkId,
+  checkRole(["Admin", "Manager"]),
+  checkOwnership,
+  Library.findLibraryById
+);
+router.patch(
+  "/edit/:id",
+  autentikasi,
+  checkId,
+  checkRole(["Admin", "Manager"]),
+  checkOwnership,
+  Library.updateLibrary
+);
+router.delete(
+  "/delete/:id",
+  autentikasi,
+  checkId,
+  checkRole(["Admin", "Manager"]),
+  checkOwnership,
+  Library.deleteLibrary
+);
 
 module.exports = router;
