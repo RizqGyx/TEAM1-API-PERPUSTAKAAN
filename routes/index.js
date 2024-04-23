28854c37d28f0e1df7123257a8d5660806c75bb6
Muhammad Rizki <106717697+RizqGyx@users.noreply.github.com>
2024-04-24 00:45:57 +0700

    Fixing: Error Merge routes/index file

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,12 @@ const swaggerDocument = require("../docs/swagger.json");
 router.use("/api-docs", swaggerUI.serve);
 router.use("/api-docs", swaggerUI.setup(swaggerDocument));
 
-const swaggerUI = require("swagger-ui-express");
-const swaggerDocument = require("../docs/swagger.json");
-
-router.use("/api-docs", swaggerUI.serve);
-router.use("/api-docs", swaggerUI.setup(swaggerDocument));
-
 // const Admin = require("./adminRouter");
 const Transaction = require("./transactionRouter");
 const Rack = require("./rackRouter");
 const Auth = require("./authRouter");
 const User = require("./userRouter");
-const Book = require("./bookRouter")
+const Book = require("./bookRouter");
 const Library = require("./libraryRouter");
 
 // router.use("/", Admin);
@@ -25,6 +19,6 @@ router.use("/api/v1/rack", Rack);
 router.use("/api/v1/auth", Auth);
 router.use("/api/v1/user", User);
 router.use("/api/v1/library", Library);
-router.use("/api/v1/book", Book)
+router.use("/api/v1/book", Book);
 
 module.exports = router;
